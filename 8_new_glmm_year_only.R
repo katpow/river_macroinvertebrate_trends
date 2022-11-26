edebfd6e1435525d20b703207feb6fa09c8c8aa0
katpow <65597055+katpow@users.noreply.github.com>
2022-11-26 15:43:47 -0500

    group&trophic_trends_GLMMs

--- 8_new_glmm_year_only.R
+++ 8_new_glmm_year_only.R
@@ -1,15 +1,11 @@
-#Running year-only models to check whether adding in typology makes a difference to results. 
-#Have to do this again because of 'year_scaled' has been changed. 
-#JASMIN script
+#Models for group-level and trophic-level trends without typology covariant  
 
 library(tidyverse)
 library(lme4)
 rm(list=ls())
-setwd("/home/users/katpow/chapter2")
-#setwd("C:/Users/katpow/OneDrive - UKCEH/Chapter 2/manipulated_data")
-type<-readRDS("typology_final.rds") 
-group_ab<- readRDS("3_year_filter_typology.rds") 
-guild_ab<- readRDS("3_year_filter_guild_typology.rds")
+type<-readRDS("gcb_typology.rds") 
+group_ab<- readRDS("gcb_fwmacro_typology.rds") 
+guild_ab<- readRDS("gcb_guild_typology.rds")
 
 #Join the data and then create models 
 
@@ -35,12 +31,12 @@ for (i in taxon_list){
   data<- group_ab %>% filter(taxon==i) %>% select(total_abundance, year_scaled, site_id, sample_id)
   mod<- glmer(total_abundance ~ year_scaled + (1|year_scaled) + (year_scaled|site_id) + (1|sample_id), data=data, family=poisson, nAGQ=0)
   if(isTRUE(class(mod)=="try-error")) {next} else {
-    saveRDS(mod, file.path("year_only_group/models", paste( gsub(" ", "_", i), "mod.rds", sep="_")))}
+    saveRDS(mod, file.path(paste( gsub(" ", "_", i), "mod.rds", sep="_")))}
 }
 
 for (i in guild_list){
   data<- guild_ab %>% filter(guild==i) %>% select(total_abundance, year_scaled, site_id, sample_id)
   mod<- glmer(total_abundance ~ year_scaled + (1|year_scaled) + (year_scaled|site_id) + (1|sample_id), data=data, family=poisson, nAGQ=0)
   if(isTRUE(class(mod)=="try-error")) {next} else {
-    saveRDS(mod, file.path("year_only_group/models", paste( gsub(" ", "_", i), "mod.rds", sep="_")))}
+    saveRDS(mod, file.path(paste( gsub(" ", "_", i), "mod.rds", sep="_")))}
 }
