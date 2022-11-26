f8564ddf621390edd41ed7115bd8d163aea536f2
katpow <65597055+katpow@users.noreply.github.com>
2022-11-26 15:47:09 -0500

    group_typology_trends

--- 7_new_glmm_models_typology.R
+++ 7_new_glmm_models_typology.R
@@ -5,9 +5,7 @@
 library(tidyverse)
 library(lme4)
 rm(list=ls())
-setwd("/home/users/katpow/chapter2")
 
-#setwd("C:/Users/katpow/OneDrive - UKCEH/Chapter 2/manipulated_data")
 type<-readRDS("gcb_typology.rds") 
 group_ab<- readRDS("gcb_fwmacro_typology.rds") 
 
@@ -30,7 +28,7 @@ for (i in taxon_list){
   data<- group_ab %>% filter(taxon==i)
   mod<- glmer(total_abundance ~ year_scaled*typology1 + (1|year_scaled) + (year_scaled|site_id) + (1|sample_id), data=data, family=poisson, nAGQ=0)
   if(isTRUE(class(mod)=="try-error")) {next} else {
-    saveRDS(mod, file.path("typology_models/typology1", paste( gsub(" ", "_", i), "mod.rds", sep="_")))}
+    saveRDS(mod, file.path("paste( gsub(" ", "_", i), "mod.rds", sep="_")))}
 }
 
 
